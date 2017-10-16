685e4c899e5e17325e7019db5bc126d6446e6ea5
CactusTribe <lefrancjoaquim@gmail.com>
2017-10-16 18:26:22 +0200

    Ajout d'une methode pour blur les images

--- data_augmentation.py
+++ data_augmentation.py
@@ -24,6 +24,12 @@ def rotate_image(mat, angle):
     rotated_mat = cv2.warpAffine(mat, rotation_mat, (bound_w, bound_h))
     return rotated_mat
 
+def change_gamma(img, ratio_gamma):
+	invGamma = 1.0 / ratio_gamma
+	table = np.array([((i / 255.0) ** invGamma) * 255
+		for i in np.arange(0, 256)]).astype("uint8")
+	return cv2.LUT(img, table)
+
 def get_images_link(path):
 	links = []
 	list_dir = sorted(os.walk(path).__next__()[1])
@@ -76,29 +82,43 @@ def change_brightness(links_liste):
 		image_name = link.split("/")[-1]
 		image_path = os.path.dirname(link)
 
-		if not image_name.startswith(("l_", "d_")):
+		if not image_name.startswith(("g6_", "g4_", "g2_", "g05_", "g025_")):
 			img = cv2.imread(link,1)
 
-			# light
-			invGamma = 1.0 / 2
-			table = np.array([((i / 255.0) ** invGamma) * 255
-				for i in np.arange(0, 256)]).astype("uint8")
-			dst = cv2.LUT(img, table)
-			cv2.imwrite("{}/l_{}".format(image_path, image_name), dst)
+			dst = change_gamma(img, 6)
+			cv2.imwrite("{}/g6_{}".format(image_path, image_name), dst)
 
-			# dark
-			invGamma = 1.0 / 0.5
-			table = np.array([((i / 255.0) ** invGamma) * 255
-				for i in np.arange(0, 256)]).astype("uint8")
-			dst = cv2.LUT(img, table)
-			cv2.imwrite("{}/d_{}".format(image_path, image_name), dst)
+			dst = change_gamma(img, 4)
+			cv2.imwrite("{}/g4_{}".format(image_path, image_name), dst)
+
+			dst = change_gamma(img, 2)
+			cv2.imwrite("{}/g2_{}".format(image_path, image_name), dst)
+
+			dst = change_gamma(img, 0.5)
+			cv2.imwrite("{}/g05_{}".format(image_path, image_name), dst)
+
+			dst = change_gamma(img, 0.25)
+			cv2.imwrite("{}/g025_{}".format(image_path, image_name), dst)
+
+def blur(links_liste):
+	for link in links_liste:
+		image_name = link.split("/")[-1]
+		image_path = os.path.dirname(link)
+
+		if not image_name.startswith(("b_")):
+			img = cv2.imread(link,1)
+			dst = cv2.blur(img,(4,4))
+			cv2.imwrite("{}/b_{}".format(image_path, image_name), dst)
 			
 
-print("# Getting images links ...")
+print("# Bluring images ...")
 images_links = get_images_link(DIR_PATH)
+blur(images_links)
+
 print("# Change brightness ...")
+images_links = get_images_link(DIR_PATH)
 change_brightness(images_links)
-print("# Getting images links ...")
+
 images_links = get_images_link(DIR_PATH)
 print("# Flipping images ...")
 apply_flip(images_links)
