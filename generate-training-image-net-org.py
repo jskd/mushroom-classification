66c7c4091e7cf023a1cbb553393afd6f55847698
jskd <git@jeromeskoda.fr>
2017-11-28 20:28:54 +0100

    Ajout de la possibilité de quiter / reprendre le telechargement

--- generate-training-image-net-org.py
+++ generate-training-image-net-org.py
@@ -14,55 +14,78 @@ def merge(patern, output):
         fo.write("".join( sorted(list(all_lines)) ))
 
 
+
+def found_in_file(fname, search):
+    logfile = open(fname, 'r')
+    loglist = logfile.readlines()
+    logfile.close()
+    for line in loglist:
+        if search in line:
+            return True
+    return False
+
 def store_raw_images(directory, urlfile):
     pic_num = 0
 
     if not os.path.exists(directory):
         os.makedirs(directory)
 
+
+    good_log= directory+'/good.txt'
+    bad_log= directory+'/bad.txt'
+
+    fgw= open(directory+'/good.txt','a')
+    fbw= open(directory+'/bad.txt','a')
+
+
     line = 1
     num_lines = sum(1 for line in open(urlfile))
     with open(urlfile) as f:
         for i in f:
-            try:
-                info = "(line:" + str(line).zfill(5) + "/"+ str(num_lines).zfill(5) +", pic-num:" + str(pic_num).zfill(5) + ", url:" + i.rstrip() + ")"
-                address = urllib.request.urlopen(i, timeout=1)
-                if(address != None):
-                    filename = directory+"/"+str(pic_num)+".jpg"
-                    urllib.request.urlretrieve(i, filename)
-                    if(magic.from_file( filename, mime=True ) == "image/jpeg"):
-                        print("[PASS] Image save " + info)
-                        pic_num += 1
-                    else:
-                        print("[FAIL] Not jpeg   " + info)
+            found= found_in_file(good_log, i) or found_in_file(bad_log, i)
 
-            except Exception as e:
-                print("[FAIL] Exception  " + info)
-                pass
+            if not found:
+                try:
+                    info = "(line:" + str(line).zfill(5) + "/"+ str(num_lines).zfill(5) +", pic-num:" + str(pic_num).zfill(5) + ", url:" + i.rstrip() + ")"
+                    address = urllib.request.urlopen(i, timeout=1)
+                    if(address != None):
+                        filename = directory+"/"+str(line)+".jpg"
+                        urllib.request.urlretrieve(i, filename)
+                        if(magic.from_file( filename, mime=True ) == "image/jpeg"):
+                            fgw.write(i)
+                            print("[PASS] Image save " + info)
+                            pic_num += 1
+                        else:
+                            os.remove(filename)
+                            fbw.write(i)
+                            print("[FAIL] Not jpeg   " + info)
+
+                except Exception as e:
+                    fbw.write(i)
+                    print("[FAIL] Exception  " + info)
+                    pass
+            else:
+                print("[PASS] Déjà dl")
             line += 1
+    fclose(fg)
+    fclose(fb)
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         directory = sys.argv[1]
         training_set_root = "./training-image-net-org/"
-        
+
         dataset_name = ["mushroom", "cactus", "flower"]
         for name in dataset_name:
             merge( training_set_root + name + '/origin/*',  training_set_root + name + '/merge.txt')
-        
-        
+
+
         if not os.path.exists(directory):
             os.makedirs(directory)
-        
+
         for name in dataset_name:
             output_directory =  directory + "/" + name + "/"
             store_raw_images(output_directory, training_set_root + name +'/merge.txt')
 
     else : print("Usage: generate-training-image-net-org <dir_out>")
-
-
-
-
-
-
