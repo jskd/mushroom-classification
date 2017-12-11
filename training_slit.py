1d193b8b53bc5cfd4bbf522d59a30147a2e0c207
jskd <git@jeromeskoda.fr>
2017-12-11 19:26:21 +0100

    Slit filename

--- training_slit.py
+++ training_slit.py
@@ -5,15 +5,25 @@ import random
 def training_slit(in_dir, out_dir, percent):
     files = glob.glob(in_dir+"/*.jpg")
     random.shuffle(files)
-    print(files)
+
+    test_set, training_set = [], []
+
+    for i, f in enumerate(files):
+        if i < len(files) * percent:
+            test_set.append(f)
+        else:
+            training_set.append(f)
+
+    print(len(test_set))
+    print(len(training_set))
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 3 :
         in_dir = sys.argv[1]
         out_dir = sys.argv[2]
-        percent = sys.argv[3]
-
+        percent = float( int(sys.argv[3]) / 100)
         training_slit(in_dir, out_dir, percent)
 
-    else : print("Usage: training_slit <dir_in> <dir_out> <percent>")
+    else:
+        print("Usage: training_slit <dir_in> <dir_out> <percent_in_training>")
